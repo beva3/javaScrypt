4de0442179c5bf97d683f75e8ba094ac3e8e247e
beva3 <mizabienvenu@gmail.com>
2024-04-30 22:12:41 +0300

    learn number and string

--- cours JS/scrypt.js
+++ cours JS/scrypt.js
@@ -1,8 +1,29 @@
 var owner = "Raphaelas";
 var n1 = 12;
 var n2 = 12.584564;
+
 console.log(n1);
 console.log(n2);
 console.log(n2.toString()); // convert to string
 console.log("hello world! welcome to javascript");
 console.log(owner);
+
+// 
+var Name = "raphael";
+console.log(Name + "==>"+Name.length);
+
+/*
+    il y a la fonction de manuipulationsde string, number
+    replace, 
+    to string
+    to number
+    index of
+ */
+ var Name = "Randrianantoanina Raphael";
+ console.log(Name.indexOf("phael")); // la debut de 'r' est au  em position
+ console.log(Name);
+ var var2 =Name.replace("Raphael", "Bienvenue");
+ console.log(var2); // la debut de 'r' est au  em position
+ var numStr = "123";
+ console.log(numStr);
+ console.log(parseInt(numStr)); // to number
