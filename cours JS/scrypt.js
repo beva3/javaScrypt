201639672a0f104cfdedf0497c0c0e9a50f156a6
beva3 <mizabienvenu@gmail.com>
2024-04-30 21:19:22 +0300

    learn text and number

--- cours JS/scrypt.js
+++ cours JS/scrypt.js
@@ -1,3 +1,8 @@
 var owner = "Raphaelas";
+var n1 = 12;
+var n2 = 12.584564;
+console.log(n1);
+console.log(n2);
+console.log(n2.toString()); // convert to string
 console.log("hello world! welcome to javascript");
 console.log(owner);
