2c1ec3ba34ee869508a68d01ab80b950245b0d38
beva3 <mizabienvenu@gmail.com>
2024-05-01 07:56:06 +0300

    learn loop

--- cours JS/scrypt.js
+++ cours JS/scrypt.js
@@ -1,16 +1,16 @@
-var owner = "Raphaelas";
-var n1 = 12;
-var n2 = 12.584564;
+// var owner = "Raphaelas";
+// var n1 = 12;
+// var n2 = 12.584564;
 
-console.log(n1);
-console.log(n2);
-console.log(n2.toString()); // convert to string
-console.log("hello world! welcome to javascript");
-console.log(owner);
+// console.log(n1);
+// console.log(n2);
+// console.log(n2.toString()); // convert to string
+// console.log("hello world! welcome to javascript");
+// console.log(owner);
 
 // 
-var Name = "raphael";
-console.log(Name + "==>"+Name.length);
+// var Name = "raphael";
+// console.log(Name + "==>"+Name.length);
 
 /*
     il y a la fonction de manuipulationsde string, number
@@ -19,14 +19,14 @@ console.log(Name + "==>"+Name.length);
     to number
     index of
  */
- var Name = "Randrianantoanina Raphael";
- console.log(Name.indexOf("phael")); // la debut de 'r' est au  em position
- console.log(Name);
- var var2 =Name.replace("Raphael", "Bienvenue");
- console.log(var2); // la debut de 'r' est au  em position
- var numStr = "123";
- console.log(numStr);
- console.log(parseInt(numStr)); // to number
+//  var Name = "Randrianantoanina Raphael";
+//  console.log(Name.indexOf("phael")); // la debut de 'r' est au  em position
+//  console.log(Name);
+//  var var2 =Name.replace("Raphael", "Bienvenue");
+//  console.log(var2); // la debut de 'r' est au  em position
+//  var numStr = "123";
+//  console.log(numStr);
+//  console.log(parseInt(numStr)); // to number
  /**
   * conditiona :
   * if(condition){
@@ -44,3 +44,26 @@ console.log(Name + "==>"+Name.length);
   *     default:
   * }
   */
+ // boucle
+ /*
+var i=0;
+
+while (i<10) {
+    console.log(i);
+    i++;
+}
+
+do{
+    console.log(i);
+    i++
+}while (i<=10);
+console.log("-================================-");
+for (var i=0; i<10; i++) {
+    console.log(i);
+}
+*/
+
+// fonction
+function sum(n1, n2) {
+    
+}
