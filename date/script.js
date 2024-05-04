640cad70e544fabc4d47fd4c7c4d42eda9d725bc
beva3 <mizabienvenu@gmail.com>
2024-05-04 08:46:57 +0300

    fini

--- date/script.js
+++ date/script.js
@@ -1,5 +1,27 @@
 let t_Now = new Date();
-const week ={};
+const WEEK ={
+    '0':'Sunday',
+    '1':'Monday',
+    '2':'Tuesday',
+    '3':'Wednesday',
+    '4':'Thursday',
+    '5':'Friday',
+    '6':'Saturday'
+};
+const MONTH={
+    '0':'January',
+    '1':'February',
+    '2':'March',
+    '3':'April',
+    '4':'May',
+    '5':'June',
+    '6':'July',
+    '7':'August',
+    '8':'September',
+    '9':'October',
+    '10':'November',
+    '11':'December'
+};
 function display_console(){
     console.log(t_Now);
     console.log('====== time ======');
@@ -28,10 +50,10 @@ function time_display(){
     document.querySelector('.h').textContent = h;
     document.querySelector('.min').textContent = m;
     document.querySelector('.sec').textContent =s;
-    document.querySelector('.day').textContent = day; // entier, faire l'adaptation apres
-    document.querySelector('.dt').textContent = dt; //
-    document.querySelector('.month').textContent = month; // entier, faire l'adaptation apres
-    document.querySelector('.year').textContent = year; //
+    document.querySelector('.day').textContent = WEEK[day]; //console.log(WEEK[day]); // entier, faire l'adaptation apres
+    document.querySelector('.dt').textContent = dt;         //
+    document.querySelector('.month').textContent = MONTH[month -1];   //console.log(MONTH[month -1]) // entier, faire l'adaptation apres
+    document.querySelector('.year').textContent = year;     //
 }
 
 document.body.onload = time_display();
