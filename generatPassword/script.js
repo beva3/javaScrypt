4208274233f9cd4303577329c35dd0b8abd5102d
beva3 <mizabienvenu@gmail.com>
2024-05-06 12:39:13 +0300

    interface backend successful

--- generatPassword/script.js
+++ generatPassword/script.js
@@ -3,5 +3,27 @@ let copyBtn = document.getElementById('copy')
 function getPassword(){
     let password = 'this is a password'
 
+    // print the password
     document.getElementById('password').value = password
+    // change style of button copy
+    copyBtn.style.background='blue'
+    copyBtn.style.color='white'
+}
+
+// copy password
+function copyPassword(){
+    let inputPassword = document.getElementById('password')
+    //console.log(inputPassword);
+    //alert('copy password')
+    // copy password
+    inputPassword.select()
+    document.execCommand('copy');
+
+    // print signature
+    copyBtn.style.background='transparent'
+    copyBtn.style.color='rgb(59, 52, 52)'
+
+    /**
+     * et maintenant il y a une probem, apres la copy, il y a encore le reset
+     */
 }
