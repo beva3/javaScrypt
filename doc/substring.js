86d6d3601ec936f770c2c0c91b34de9c584bf0af
beva3 <mizabienvenu@gmail.com>
2024-05-06 22:37:00 +0300

    comprendre substring

---
+++ doc/substring.js
@@ -0,0 +1,13 @@
+/** 
+ * La méthode substring() retourne une sous-chaîne 
+ * de la chaîne courante, entre un indice de début et un indice de fin.
+*/
+const me ='Raphael'
+
+console.log(me.substring(0,1)); // R
+console.log(me.substring(0,2)); // Ra
+console.log(me.substring(1,1)); // void
+
+console.log(me.substring(1,2)); // a
+
+console.log(me.substring(1,3)); // ap
