68c840bba7f1dfe5e24fbeb5a5bace47e11c00ec
beva3 <mizabienvenu@gmail.com>
2024-06-15 11:21:11 +0300

    revivion

---
+++ manisa/script.js
@@ -0,0 +1,10 @@
+let input_N = document.querySelector('input');
+let test = document.querySelector('body');
+console.log(test);
+console.log(input_N);
+let result = document.getElementById('resultat');
+result.innerHTML = input_N.value;
+console.log(result);
+
+var tab = document.getElementById('resultat');
+console.log(tab);
---
+++ text-editor/scipt.js
@@ -0,0 +1,10 @@
+let btn_save = document.getElementById('btn_save');
+console.log(btn_save);
+
+
+
+
+
+btn_save.addEventListener('click', ()=>{
+    alert('Save');
+});
